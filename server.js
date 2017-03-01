fa5d9c850a69a6d968f942b92ad9de8df28ac29c
aranajhonny <jhonnyjosearana@gmail.com>
2017-03-01 13:48:24 -0430

    fix serve

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ const { createServer } = require("http");
 const { parse } = require("url");
 const micro = require("micro");
 const next = require("next");
-
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
-
 app.prepare().then(() => {
   micro(async (req, res) => {
     const parsedUrl = parse(req.url, true);
