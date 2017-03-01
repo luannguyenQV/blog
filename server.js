784c6666e54eec62678869ed41ec49b342710a1b
aranajhonny <jhonnyjosearana@gmail.com>
2017-03-01 11:26:20 -0430

    add micro

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { createServer } = require("http");
 const { parse } = require("url");
+const micro = require("micro");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
@@ -7,12 +8,9 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
+  micro(async (req, res) => {
     const parsedUrl = parse(req.url, true);
     const { pathname, query } = parsedUrl;
-    handle(req, res, parsedUrl);
-  }).listen(3000, err => {
-    if (err) throw err;
-    console.log("> Ready on http://localhost:3000");
-  });
+    await handle(req, res, parsedUrl);
+  }).listen(3000);
 });
