4fa83c1208690e5ddd10ff0dd29c3108a87f432c
aranajhonny <jhonnyjosearana@gmail.com>
2017-03-02 14:48:22 -0430

    :fire:

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  webpack: function(config) {
-    config.resolve.alias = {
-      react: "preact-compat/dist/preact-compat",
-      "react-dom": "preact-compat/dist/preact-compat"
-    };
-    return config;
-  }
-};
